1b8471cd2ef67fc13bdfe16ad2ff5e3a00a88076
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-10-25 14:47:55 -0300

    Upload JS
    Adicionando index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,49 @@
 
+	var saidaCaixa = document.querySelector('[data-valor-caixa]');
+	var comprovantePagamento = document.querySelector('[data-comprovante-pagamento]');
+	var valorHTML = document.querySelector('[data-painel-valor]');
+
+	var compra = {
+
+	caixa: 0,
+
+	movimentacao: [],
+
+	valorRecebido: function() {
+
+		this.caixa += Number(valorHTML.value);
+		this.movimentacao.push("Valor recebido " + valorHTML.value);
+		valorHTML.value = "";
+		valorHTML.focus();
+	},
+
+	valorPagamento: function() {
+
+		var valor = Number(valorHTML.value);
+
+		if(valor>this.caixa) {
+			alert("Valor recebido é menor do que o de pagamento!");
+
+		} else {
+			this.caixa -= valor;
+			this.movimentacao.push("Valor do pagamento " + valor + "<br>");
+	}
+		saidaCaixa = this.caixa;
+		this.movimentacao.push("Valor do troco " + saidaCaixa + "<br><br>" + "Obrigado pela preferência!");
+		valorHTML.value = "";
+		valorHTML.focus();
+
+	},
+
+	comprovantePagamento: function() {
+
+		comprovantePagamento.innerHTML = "** Nota para Retirada de Produto **" + "<br><br>";
+
+		for(i=0; i<=this.movimentacao.length-1; i++) {
+			comprovantePagamento.innerHTML += this.movimentacao[i] + "<br>";
+
+	}
+			
+	}
+
+	}
